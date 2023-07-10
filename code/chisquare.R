3637b8e0b4adab98a605675762b26c59a88d47bf
ecologytate <th522@cornell.edu>
2023-07-10 13:12:05 -0700

    updated chisq

--- code/chisquare.R
+++ code/chisquare.R
@@ -10,13 +10,64 @@ T13 <- rep("T13", 18)
 T15 <- rep("T15", 18)
 T18 <- rep("T18", 18)
 
-#add temp column
+#add temp column to summed-original-72 data frame
 summed_original <- summed_original %>%
   mutate(temp = c(T10, T13, T15, T18)) %>%
   relocate(temp, .before = treat_well)
 
-# create ratio of female/ total, male/ total within each temp
-# yates correction to modify test statistic when 2 values/ proportions
-# run 4 separate chisq tests
+# Summing count data by treatments
+sum_data_fem <- aggregate(summed_original$female ~ summed_original$temp, summed_original, sum)
+sum_data_mal <- aggregate(summed_original$male ~ summed_original$temp, summed_original, sum)
+sum_data_all <- aggregate(summed_original$female+summed_original$male ~ summed_original$temp, summed_original, sum)
 
-chisq.test(x = c())
+# Display the new data frame
+print(sum_data_fem)
+print(sum_data_mal)
+print(sum_data_all)
+
+
+# Combine aggregated data into a new data frame
+combined_data <- merge(merge(sum_data_fem, sum_data_mal, by = "summed_original$temp"), sum_data_all, by = "summed_original$temp")
+
+# Display the new data frame
+print(combined_data)
+
+# Create new contingency table to run Chi-sq
+observed <- matrix(c(347, 450, 456, 693, 552, 739, 632, 649), nrow = 4, ncol = 2, byrow = TRUE) #observed frequencies
+colnames(observed) <- c("Female", "Male")
+rownames(observed) <- c("T10", "T13", "T15", "T18")
+
+#check matrix is correct
+print(observed)
+
+# Define expected probabilities
+expected_prob <- c(0.5, 0.5)
+
+# Run chi-squared test with expected probability of 0.5
+result <- chisq.test(observed, p = expected_prob)
+
+# Print the result
+print(result)
+result$expected
+result$residuals
+
+#alternative test for p = 0.5
+# Specify the expected proportion
+expected_proportion <- 0.5
+
+# Calculate the expected frequencies
+expected <- expected_proportion * rowSums(observed)
+
+print(expected)
+
+# Perform the chi-square test of independence
+result2 <- chisq.test(observed, p = expected_proportion)
+
+# Print the result
+print(result2)
+
+# Run pairwise chi-square tests for multiple comparisons
+pairwise_result <- pairwise.chisq.test(observed)
+
+# Print the pairwise comparison result
+print(pairwise_result)
